f7fc8fac82c565649d8d30c50b8d2d4956a4f045
Andres <andresalv7@gmail.com>
2022-05-10 08:53:28 -0300

    creaCliente endpoint creation

--- src/main/java/com/intercorpretail/challenge/controller/ChallengeController.java
+++ src/main/java/com/intercorpretail/challenge/controller/ChallengeController.java
@@ -1,8 +1,23 @@
 package com.intercorpretail.challenge.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.intercorpretail.challenge.model.Client;
+import com.intercorpretail.challenge.repository.DaoClient;
 
 @Controller
 public class ChallengeController {
+	
+	@Autowired
+	private DaoClient daoClient;
 
+	@PostMapping(path="/creaCliente")
+	private ResponseEntity<Object> crearCliente(@RequestBody Client client) {
+		daoClient.save(client);
+		return ResponseEntity.ok().body(client);
+	}
 }
