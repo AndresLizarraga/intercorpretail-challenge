c609d1b7a02462c611a55adb4029336519853eb3
Andres <andresalv7@gmail.com>
2022-05-10 08:19:21 -0300

    ClientService methods creation

--- src/main/java/com/intercorpretail/challenge/service/ClientService.java
+++ src/main/java/com/intercorpretail/challenge/service/ClientService.java
@@ -1,5 +1,13 @@
 package com.intercorpretail.challenge.service;
 
+import java.util.List;
+
+import com.intercorpretail.challenge.model.Client;
+
 public interface ClientService {
 
+	int calculateAgeAverage(Client client);
+	
+	int calcularStandardDeviation(List<Client> clients);
+	
 }
--- src/main/java/com/intercorpretail/challenge/service/impl/ClientServiceImpl.java
+++ src/main/java/com/intercorpretail/challenge/service/impl/ClientServiceImpl.java
@@ -1,5 +1,22 @@
 package com.intercorpretail.challenge.service.impl;
 
-public class ClientServiceImpl {
+import java.util.List;
+
+import com.intercorpretail.challenge.model.Client;
+import com.intercorpretail.challenge.service.ClientService;
+
+public class ClientServiceImpl implements ClientService{
+
+	@Override
+	public int calculateAgeAverage(Client client) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int calcularStandardDeviation(List<Client> clients) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
